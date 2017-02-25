968aea9498b5a79176169c6d7ead71b2edce08ac
MaxLin <maxylin@gmail.com>
2017-02-25 15:51:26 -0600

    Add routing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import angular from 'angular';   
 import 'angular-route';
+import * as firebase from "firebase";
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDiL1XGJxQqqd8WCnwx6FQvzapSphklSmk",
+  authDomain: "tardy-ccd34.firebaseapp.com",
+  databaseURL: "https://tardy-ccd34.firebaseio.com",
+  storageBucket: "tardy-ccd34.appspot.com",
+  messagingSenderId: "959407744332"
+};
+firebase.initializeApp(config);
+
 
 class MyCtrl {
     constructor() {
@@ -7,5 +19,14 @@ class MyCtrl {
     }
 }
 
-angular.module("myApp", [])
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "./home.html"
+            })
+            .otherwise({
+                templateUrl: "./404.html"
+            })
+    })
     .controller("MyCtrl", MyCtrl);
