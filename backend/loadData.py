f8706bec04c2db07175e31b3496b999fa7fb5d7a
Kevin Xia <kevinmxia@gmail.com>
2017-02-25 17:38:45 -0500

    Added in function to add data entries

---
+++ backend/loadData.py
@@ -0,0 +1,23 @@
+from firebase import firebase
+
+
+import pyrebase
+
+config = {
+    "apiKey": "AIzaSyDiL1XGJxQqqd8WCnwx6FQvzapSphklSmk",
+    "authDomain": "tardy-ccd34.firebaseapp.com",
+    "databaseURL": "https://tardy-ccd34.firebaseio.com",
+    "storageBucket": "tardy-ccd34.appspot.com",
+    "serviceAccount": "tardy-ccd34-firebase-adminsdk-lxytz-ab3d23dae1.json"
+    }
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
+def addEntry(userId, target, expected, actual):
+    db.child('users/' + userId).child(target).push([expected, actual]);
+
+#Test
+addEntry("kijZjJJ5ozPZxfeHYfjh3zd3TUh1", "Janice", "80000", "90000")
+
+
