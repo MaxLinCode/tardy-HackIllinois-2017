39a42eb90a4899659600c23cd6c412a3ffc4a6c1
Kevin Xia <kevinmxia@gmail.com>
2017-02-25 20:26:35 -0500

    added weighted average system

--- backend/loadData.py
+++ backend/loadData.py
@@ -19,9 +19,25 @@ def addEntry(userId, target, expected, actual):
 def getEntry(userId, target):
     return db.child('users/' + userId).child(target).get().val()
 
+def predict(userId, target, expected):
+    vallist = list(getEntry(userId, target).values())
+    total = 0
+    count = 0
+    mult = 1
+    for item in vallist:
+        diff = int(item[1]) - int(item[0])
+        if diff < (0 - 43200):
+            diff = diff + 86400
+        diff = diff * mult
+        total += diff
+        count += mult
+        mult = mult * 1.01
+    avg = total / count
+    print(avg)
+    return expected - avg
+
+
 #Test
-#addEntry("kijZjJJ5ozPZxfeHYfjh3zd3TUh1", "Janice", "80000", "90000")
+addEntry("kijZjJJ5ozPZxfeHYfjh3zd3TUh1", "Janice", "68400", "68400")
 
-data = getEntry('kijZjJJ5ozPZxfeHYfjh3zd3TUh1', "Janice")
-
-print(data.val())
+print(predict('kijZjJJ5ozPZxfeHYfjh3zd3TUh1', "Janice", 35000))
