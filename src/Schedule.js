e2c0346d6a20591e05231b50a8689337d48d2f37
avincent6 <vincent6@purdue.edu>
2017-02-26 00:23:03 -0600

    schedule form

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,12 +1,54 @@
 import React from 'react'
+var poop =  "Poop";
 
 class ScheduleForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {username: '', password: ''};
+        this.state = {
+          username: '',
+          full_name: '',
+          expected_time: [
+            {
+              hours: '',
+              minutes: '',
+              isPm: 'am',
+              seconds: '',
+            }],
+          arrival_time: [
+            {
+              hours: '',
+              minutes: '',
+              isPm: 'am',
+              seconds: ''
+            }],
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    getSeconds(event) {
+      var total;
+      const name = event.target.name;
+      total += this.state.expected_time.hours * 1200;
+      total += this.state.expected_time.minutes * 60;
+      if (isAm == "pm") {
+        total += 12*1200;
+      }
+      this.setState({
+        [name.seconds]: total
+      });
+    }
+    makeFalse(event) {
+      const name = event.target.name;
+      this.setState({
+        [name.isPm]: "pm"
+      });
+
+    }
+
+    callGetSeconds() {
+      this.handleChangethis,
+      this.getSeconds
     }
 
     handleChange(event) {
@@ -18,7 +60,7 @@ class ScheduleForm extends React.Component {
     }
 
     handleSubmit(event) {
-        alert('User: ' + this.state.username + '\nPass: ' + this.state.password);
+        alert('User: ' + this.state.username + '\nExpected Time: ' + this.state.expected_time + '\nArrival Time: ');
         event.preventDefault();
     }
 
@@ -28,11 +70,34 @@ class ScheduleForm extends React.Component {
         <div className='sign-in'>
         <h1>Schedule</h1>
         <form onSubmit={this.handleSubmit} >
-            <label>Username</label>
-            <input className='input' type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-            <br />
-            <label>Password</label>
-            <input className='input' type="text" name="password" value={this.state.password} onChange={this.handleChange} />
+            <label><p>Full Name</p></label>
+            <input className='input' type="text" name="full_name" value={this.state.name} onChange={this.handleChange} />
+
+            <label><p>Expected Time</p></label>
+            <div className='time-capsule'>
+            <input className='input time in' maxLength="2" value={this.state.expected_time.hours} onChange={this.handleChange}/>
+            <p className='in'>&nbsp;:&nbsp;</p>
+            <input className='input time in' maxLength="2" value={this.state.expected_time.minutes} onChange={this.handleChange}/>
+            <div className='style-select'>
+            <select>
+              <option className='in' value={this.state.arrival_time.isPm} onClick={this.makeFalse}>AM</option>
+              <option className='in' value={this.state.arrival_time.isPm}>PM</option>
+            </select>
+            </div>
+            </div>
+
+            <label><p>Actual Arrival Time</p></label>
+            <div className='time-capsule'>
+            <input className='input time in' maxLength="2" value={this.state.arrival_time.hours} onChange={this.callGetSeconds}/>
+            <p className='in'>&nbsp;:&nbsp;</p>
+            <input className='input time in' maxLength="2" value={this.state.arrival_time.minutes} onChange={this.callGetSeconds}/>
+            <div className='style-select'>
+            <select>
+              <option className='in' value={this.state.arrival_time.isPm} onClick={this.makeFalse}>AM</option>
+              <option className='in' value={this.state.arrival_time.isPm}>PM</option>
+            </select>
+            </div>
+            </div>
         <input className='submit btn' type="submit" value="Submit" />
         </form>
         </div>
