b596836bebb36c4f1f69bdfa4d9e32dc9fb5f5d5
MaxLin <maxylin@gmail.com>
2017-02-26 00:35:42 -0600

    Merge

--- src/Login.js
+++ src/Login.js
@@ -6,7 +6,6 @@ class LoginForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {email: '', password: ''};
-
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
--- src/app.js
+++ src/app.js
@@ -1,29 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router';
 import * as firebase from "firebase";
+import { logout } from './Auth'
+import { firebaseAuth } from './config/fireConstants'
 
-import Main from './Main'
+import Home from './Home'
 import NotFound from './NotFound'
 import About from './About'
 import Login from './Login'
 import Schedule from './Schedule'
 import SignIn from './SignIn'
 
-ReactDOM.render((
-    <Router history={hashHistory}>
-      <Route path="/" component={Main}>
-        <Route path="/about" component={About}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/schedule" component={Schedule}/>
-        <Route path="/signin" component={SignIn}/>
-      </Route>
-      <Route path="*" component={NotFound}/>
-    </Router>
-), document.getElementById('app'));
-
-firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in.
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {"authed": "false"};
+    this.handleLogout = this.handleLogout.bind(this)
   }
-});
+
+  componentWillMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      this.setState({ authed: "true" });
+    }
+    else {
+      this.setState({ authed: "false" });
+    }
+    }.bind(this));
+  }
+
+  componentDidMount() {
+    this.removeListener();
+  }
+
+  requireAuth(nextState, replace) {
+    var user = firebase.auth().currentUser;
+    if (!user) {
+      replace({
+        pathname: '/login'
+      })
+    }
+}
+
+  handleLogout() {
+    logout();
+    this.setState({authed: false});
+  }
+  
+  render() {
+    return (
+      <div>
+        <Router history={hashHistory}>
+          <Route path="/" component={Home} >
+            <IndexRoute component={Login} />
+            <Route path="/about" component={About} />
+            <Route path="/login" component={Login} />
+            <Route path="/schedule" component={Schedule} onEnter={this.requireAuth}/>
+          </Route>
+          <Route path="*" component={NotFound}/>
+        </Router>
+        <button onClick={this.handleLogout}>Logout</button>
+      </div>
+    );
+  }
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
+/*
+ <button onClick={() => {
+          this.setState({authed: 'true'})
+          logout()
+        }}>Logout</button>
+*/
