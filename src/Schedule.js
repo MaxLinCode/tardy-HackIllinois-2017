eb9d265405891698484439e71a67404d206ed42e
MaxLin <maxylin@gmail.com>
2017-02-26 03:22:37 -0600

    Fix up schedule parsing

--- src/Schedule.js
+++ src/Schedule.js
@@ -7,20 +7,20 @@ class ScheduleForm extends React.Component {
         super(props);
         this.state = {
           full_name: '',
-          expected_time: [
+          expected_time: 
             {
               hours: '',
               minutes: '',
               isPm: 'am',
               seconds: '',
-            }],
-          arrival_time: [
+            },
+          arrival_time: 
             {
               hours: '',
               minutes: '',
               isPm: 'am',
               seconds: ''
-            }],
+            },
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -55,20 +55,54 @@ class ScheduleForm extends React.Component {
     handleChange(event) {
         const target = event.target;
         const name = target.name;
-        if (name == 'expected_time') {
-          this.setState
-        } 
-        else if (name == 'arrival_time') {
-          
-        } else {
+        var copy;
+        if (name == 'expected_time_hours') {
+          copy = this.state.expected_time;
+          copy.hours = target.value;
+          this.setState({
+            expected_time: copy
+          });
+        } else if (name == 'expected_time_minutes') {
+          copy = this.state.expected_time;
+          copy.minutes = target.value;
+          this.setState({
+            expected_time: copy
+          });
+        } else if (name == 'arrival_time_hours') {
+          copy = this.state.arrival_time;
+          copy.hours = target.value;
+          this.setState({
+            arrival_time: copy
+          });
+        } else if (name == 'arrival_time_minutes') {
+          copy = this.state.arrival_time;
+          copy.minutes = target.value;
+          this.setState({
+            arrival_time: copy
+          });
+        } else if (name == 'expected_time_isPM') {
+          copy = this.state.expected_time;
+          copy.isPM = target.value;
+          this.setState({
+            expected_time: copy
+          });
+        } else if (name == 'arrival_time_isPM') {
+          copy = this.state.arrival_time;
+          copy.isPM = target.value;
+          console.log(target.value)
+          this.setState({
+            arrival_time: copy
+          });
+        }
+        else {
           this.setState({
               [name]: target.value
           });
-        }
+      }
     }
 
     handleSubmit(event) {
-        alert('User: ' + this.state.full_name + '\nExpected Time: ' + this.state.expected_time.hours + '\nArrival Time: ');
+        alert('User: ' + this.state.full_name + '\nExpected Time: ' + this.state.expected_time.hours + ' ' + this.state.expected_time.minutes+ ' ' + this.state.expected_time.isPm + '\nArrival Time: ' + this.state.arrival_time.hours + ' ' + this.state.arrival_time.minutes + ' ' + this.state.expected_time.isPm);
         event.preventDefault();
     }
 
@@ -83,26 +117,26 @@ class ScheduleForm extends React.Component {
 
             <label><p>Expected Time</p></label>
             <div className='time-capsule'>
-            <input className='input time in' maxLength="2" name="expected_time" value={this.state.expected_time.hours} onChange={this.handleChange}/>
+            <input className='input time in' maxLength="2" name="expected_time_hours" value={this.state.expected_time.hours} onChange={this.handleChange}/>
             <p className='in'>&nbsp;:&nbsp;</p>
-            <input className='input time in' maxLength="2" name="expected_time" value={this.state.expected_time.minutes} onChange={this.handleChange}/>
+            <input className='input time in' maxLength="2" name="expected_time_minutes" value={this.state.expected_time.minutes} onChange={this.handleChange}/>
             <div className='style-select'>
             <select>
-              <option className='in' value={this.state.arrival_time.isPm} onClick={this.callGetSeconds}>AM</option>
-              <option className='in' value={this.state.arrival_time.isPm} onClick={this.getSeconds}>PM</option>
+              <option className='in' value={this.state.expected_time.isPm} name="expected_time_isPm" onClick={this.callGetSeconds} onChange={this.handleChange}>AM</option>
+              <option className='in' value={this.state.expected_time.isPm} name="expected_time_isPm" onClick={this.getSeconds} onChange={this.handleChange}>PM</option>
             </select>
             </div>
             </div>
 
             <label><p>Actual Arrival Time</p></label>
             <div className='time-capsule'>
-            <input className='input time in' maxLength="2" name="arrival_time" value={this.state.arrival_time.hours} onChange={this.handleChange}/>
+            <input className='input time in' maxLength="2" name="arrival_time_hours" value={this.state.arrival_time.hours} onChange={this.handleChange}/>
             <p className='in'>&nbsp;:&nbsp;</p>
-            <input className='input time in' maxLength="2" name="arrival_time" value={this.state.arrival_time.minutes} onChange={this.handleChange}/>
+            <input className='input time in' maxLength="2" name="arrival_time_minutes" value={this.state.arrival_time.minutes} onChange={this.handleChange}/>
             <div className='style-select'>
             <select>
-              <option className='in' value={this.state.arrival_time.isPm} onClick={this.callGetSeconds}>AM</option>
-              <option className='in' value={this.state.arrival_time.isPm} onClick={this.getSeconds}>PM</option>
+              <option className='in' value={this.state.arrival_time_isPm} name="arrival_time_isPm" onClick={this.callGetSeconds} onChange={this.handleChange}>AM</option>
+              <option className='in' value={this.state.arrival_time_isPm} name="arrival_time_isPm" onClick={this.getSeconds} onChange={this.handleChange}>PM</option>
             </select>
             </div>
             </div>
